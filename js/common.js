cc90b3775fa31453c2651ff85a31a8a2c4bcade1
n.yerdenqyzy <n.yerdenqyzy@bugingroup.kz>
2018-04-19 12:46:49 +0600

    Signed-off-by: n.yerdenqyzy <n.yerdenqyzy@bugingroup.kz>

--- js/common.js
+++ js/common.js
@@ -5,5 +5,19 @@ $(document).ready(function () {
     $(".ic-close-menu").click(function(){
         $(".menu-left").fadeOut();
     });
+    $('.slid-banners').slick({
+        autoplay:true,
+        autoplaySpped:1500,
+        dots:false,
+        arrows:false,
+        fade:true
+    });
+    setTimeout(function() {
+        $('.mod-fb').fadeIn(500)
+    }, 2000);
+    $(".icon-close").click(function(){
+        $(".mod-fb").fadeOut();
+    });
 });
+
 
